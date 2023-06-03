743ea8068c9e110f5300597c95994382bcbb6c77
Dzhusuev Adilet <adilet.djusuev@gmail.com>
2023-06-03 16:04:53 +0600

    Добавил изменения: при нажатии на кнопку появляется в левом блоке имя, цена и количество товара

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,61 @@ import drinkImage from '../src/assets/icons/drink.png';
 
 const App = () => {
   const Items: IItems[] = [
-    { id: nanoid(), name: 'Humburger', price: 80, image: foodImage},
-    { id: nanoid(), name: 'Cheeseburger', price: 90, image: foodImage},
+    { id: nanoid(), name: 'Humburger', price: 80, image: foodImage },
+    { id: nanoid(), name: 'Coffee', price: 70, image: drinkImage },
     { id: nanoid(), name: 'Fries', price: 45, image: foodImage },
     { id: nanoid(), name: 'Tea', price: 50, image: drinkImage },
-    { id: nanoid(), name: 'Coffee', price: 70, image: drinkImage },
+    { id: nanoid(), name: 'Cheeseburger', price: 90, image: foodImage},
     { id: nanoid(), name: 'Cola', price: 40, image: drinkImage },
   ];
 
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState<{ name: string, count: number; price: number }[]>([]);
 
-  const handleItemClick = (index: number) => {
-    const ordersCopy = [...orders];
+  const handleItemClick = (itemName: string, price: number) => {
+    const realOrder = orders.find((order) => order.name === itemName);
 
+    if (realOrder) {
+      const updatedOrders = orders.map((order) =>
+        order.name === itemName
+          ? { ...order, count: order.count + 1, price: order.price + price } 
+          : order
+      );
+      setOrders(updatedOrders);
+    } else {
+      const newOrder = { name: itemName, count: 1, price: price };
+      setOrders([...orders, newOrder]);
+    }
   };
+
 
   const itemButtons = Items.map((item, index) => (
     <div className='itemComponents'>
-      <button className='itemBtn' key={item.id}>
+      <button className='itemBtn' key={item.id} onClick={() => handleItemClick(item.name, item.price)}>
         <img className='btnIcons' src={item.image} alt="" />
         {item.name}
+        <p>
+          KGS {item.price}
+        </p>
       </button>
     </div>
+  ));
+
+   const orderedItems = orders.map((order) => (
+    <div key={order.name}>
+      {order.name} ---------- x{order.count} --- {order.price} KGS
+     </div>
   ));
 
   return (
     <div className="App">
       <div className='main-menu'>
-        <div className='leftBlock'></div>
+        <div className='leftBlock'>
+          {orderedItems}
+          <strong className='totalPrice'>Total price: 0</strong>
+        </div>
 
         <div className='rightBlock'>
-          <div>
+          <div className='gridContainer'>
             {itemButtons}
           </div>
         </div>
